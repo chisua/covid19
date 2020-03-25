13544cbe37a6ec6acf74f7c9eeb17e483ffc4c36
chisua <chisuakun@gmail.com>
2020-03-25 17:52:45 +0900

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -81,7 +81,7 @@ export default Vue.extend({
       Data,
       headerItem: {
         icon: 'mdi-chart-timeline-variant',
-        title: this.$t('都内の最新感染動向')
+        title: this.$t('都内の最新感染動向 test')
       },
       newsItems: News.newsItems
     }
